96aab1bc6aaa37253765c830a10de248fc298a4e
marklin <2273990296@qq.com>
2021-11-18 21:18:34 +0800

    [2021-11-18 星期四]:commit to master

---
+++ open-algorithm-analysis/src/main/java/com/algorithm/question/QuestionSumAnalysis.java
@@ -0,0 +1,16 @@
+package com.algorithm.question;
+
+/**
+ * <p>封装GreeCloud项目QuestionSumAnalysis类.<br></p>
+ * <p>//TODO.<br></p>
+ *
+ * @author Powered by marklin 2021-11-18 20:59
+ * @version 1.0.0
+ * <p>Copyright © 2018-2021 Pivotal Cloud Technology Systems Incorporated. All rights reserved.<br></p>
+ */
+public class QuestionSumAnalysis {
+
+    public static void main(String[] args) {
+        System.out.println("1111");
+    }
+}
